8c33334fef24fe033513d7c13ef859f5d5624d25
C23598 <C23598@L-PC0UFPZ8.us.int.rci.com>
2020-05-24 20:47:59 -0400

    change to build dirr

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd /var/bharath-todo
+cd /var/bharath-todo/target
 sudo /usr/bin/java -jar -Dserver.port=8080 \
     rest-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
