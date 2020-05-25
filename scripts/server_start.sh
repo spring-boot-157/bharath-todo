dd9f87dac5363d9811b3a2920b02cb3c9b450536
C23598 <C23598@L-PC0UFPZ8.us.int.rci.com>
2020-05-24 20:47:24 -0400

    changes to build dir

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd /home/ec2-user/server
+cd /var/bharath-todo
 sudo /usr/bin/java -jar -Dserver.port=8080 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+    rest-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
